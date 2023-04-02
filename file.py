acd4130fce5591a48b27a6665ba7ac3daf057761
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-04-02 01:54:18 +0100

    trim data + prepare for seq2seq model

--- file.py
+++ file.py
@@ -1,10 +1,17 @@
 import codecs
 import csv
+import itertools
 import re,os,json
+from random import random
+import random
+import torch as t
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import keras
+import unicodedata
+from opt_einsum.backends import torch
 
 #printing data
 corpus_name = "movie-corpus"
@@ -54,6 +61,7 @@ def extractSentencePairs(conversations):
     for conversation in conversations.values():
         # Iterate over all the lines of the conversation
         for i in range(len(conversation["lines"]) - 1):  # We ignore the last line (no answer for it)
+            #strip (delete white space )
             inputLine = conversation["lines"][i]["text"].strip()
             targetLine = conversation["lines"][i+1]["text"].strip()
             # Filter wrong samples (if one of the lists is empty)
@@ -90,19 +98,18 @@ print("\nSample lines from file:")
 # putting pairs into variable not a file
 pairs=extractSentencePairs(conversations)
 print(pairs)
+
 # Tokenize text data
 tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
 tokenizer.fit_on_texts([pair[0] for pair in pairs] + [pair[1] for pair in pairs])
 input_sequences = tokenizer.texts_to_sequences([pair[0] for pair in pairs])
 target_sequences = tokenizer.texts_to_sequences([pair[1] for pair in pairs])
 
-# Create vocabulary
+"""
+# trim ddata  , Create vocabulary
 word2idx = tokenizer.word_index
 idx2word = {v: k for k, v in word2idx.items()}
 vocab_size = len(word2idx) + 1
-
-
-
 
 print("Number of sentence pairs:", len(input_sequences))
 
@@ -123,3 +130,225 @@ print("\nIndex to word mapping sample:")
 for i, word in enumerate(idx2word):
     if i < 10:
         print(f"{i}: {word}")
+"""
+
+
+
+
+#trim data :
+# Default word tokens
+PAD_token = 0  # Used for padding short sentences
+SOS_token = 1  # Start-of-sentence token
+EOS_token = 2  # End-of-sentence token
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3  # Count SOS, EOS, PAD
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
+
+        keep_words = []
+
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
+
+        # Reinitialize dictionaries
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3 # Count default tokens
+
+        for word in keep_words:
+            self.addWord(word)
+MAX_LENGTH = 10  # Maximum sentence length to consider
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+#separates the base character and diacritical mark into separate characters.
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s):
+    #removes diacritics from the string, and then lower and strip are
+    # used to convert the string to lowercase and remove any leading/trailing
+    # whitespace.
+    s = unicodeToAscii(s.lower().strip())
+    #This step adds a space before any punctuation marks (. ! ?) so
+    # that they can be treated as separate tokens in the later stages
+    # of the NLP pipeline
+    s = re.sub(r"([.!?])", r" \1", s)
+    #replaces any character that is not a letter, punctuation mark or
+    # whitespace with a single space. This is done to simplify the text
+    # and reduce the number of unique tokens.
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    #eplaces multiple spaces with a single space and removes any leading/trailing
+    # whitespace that may have been introduced in the previous steps.
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+# Read query/response pairs and return a voc object
+
+for i in range(len(pairs)):
+    pairs[i][0] = normalizeString(pairs[i][0])
+    pairs[i][1] = normalizeString(pairs[i][1])
+voc = Voc(corpus_name)
+
+# Returns True iff both sentences in a pair 'p' are under the MAX_LENGTH threshold
+def filterPair(p):
+    # Input sequences need to preserve the last word for EOS token
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+# Filter pairs using filterPair condition
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+# Using the functions defined above, return a populated voc object and pairs list
+def loadPrepareData(voc , pairs ):
+    print("Start preparing training data ...")
+
+    print("Read {!s} sentence pairs".format(len(pairs)))
+    pairs = filterPairs(pairs)
+    print("Trimmed to {!s} sentence pairs".format(len(pairs)))
+    print("Counting words...")
+    for pair in pairs:
+        voc.addSentence(pair[0])
+        voc.addSentence(pair[1])
+    print("Counted words:", voc.num_words)
+    return voc, pairs
+
+
+# Load/Assemble voc and pairs
+save_dir = os.path.join("data", "save")
+voc, pairs = loadPrepareData(voc,pairs)
+# Print some pairs to validate
+print("\npairs:")
+for pair in pairs[:10]:
+    print(pair)
+
+MIN_COUNT = 3    # Minimum word count threshold for trimming
+def trimRareWords(voc, pairs, MIN_COUNT):
+    # Trim words used under the MIN_COUNT from the voc
+    voc.trim(MIN_COUNT)
+    # Filter out pairs with trimmed words
+    keep_pairs = []
+    for pair in pairs:
+        input_sentence = pair[0]
+        output_sentence = pair[1]
+        keep_input = True
+        keep_output = True
+        # Check input sentence
+        for word in input_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_input = False
+                break
+        # Check output sentence
+        for word in output_sentence.split(' '):
+            if word not in voc.word2index:
+                keep_output = False
+                break
+
+        # Only keep pairs that do not contain trimmed word(s) in their input or output sentence
+        if keep_input and keep_output:
+            keep_pairs.append(pair)
+
+    print("Trimmed from {} pairs to {}, {:.4f} of total".format(len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
+    return keep_pairs
+# Trim voc and pairs
+pairs = trimRareWords(voc, pairs, MIN_COUNT)
+def indexesFromSentence(voc, sentence):
+    return [voc.word2index[word] for word in sentence.split(' ')] + [EOS_token]
+
+#The function returns a list of sequences, where each sequence
+# is zero-padded to the length of the longest sequence in the input list.
+def zeroPadding(l, fillvalue=PAD_token):
+
+    #itertools.zip_longest function works by taking the elements of each
+    # sequence in the input list in turn and grouping them together. If
+    # the sequences are of different lengths, the missing values are filled
+    # with the specified fillvalue. The * in zip_longest(*l) is used to
+    # unpack the list of sequences, so that each sequence is passed as a
+    # separate argument to the zip_longest function.
+    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
+
+def binaryMatrix(l, value=PAD_token):
+    m = []
+    for i, seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+    return m
+
+# Returns padded input sequence tensor and lengths
+def inputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    lengths = t.tensor([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    padVar = t.LongTensor(padList)
+    return padVar, lengths
+
+# Returns padded target sequence tensor, padding mask, and max target length
+def outputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    max_target_len = max([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    mask = binaryMatrix(padList)
+    mask = t.BoolTensor(mask)
+    padVar = t.LongTensor(padList)
+    return padVar, mask, max_target_len
+
+# Returns all items for a given batch of pairs
+def batch2TrainData(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp, lengths = inputVar(input_batch, voc)
+    output, mask, max_target_len = outputVar(output_batch, voc)
+    return inp, lengths, output, mask, max_target_len
+
+
+# Example for validation
+small_batch_size = 5
+batches = batch2TrainData(voc, [random.choice(pairs) for _ in range(small_batch_size)])
+input_variable, lengths, target_variable, mask, max_target_len = batches
+
+print("input_variable:", input_variable)
+print("lengths:", lengths)
+print("target_variable:", target_variable)
+print("mask:", mask)
+print("max_target_len:", max_target_len)
+
